dea52f57192ce269694847603ea4f907e5e0b2b4
Ramis <ramismubick@gmail.com>
2022-07-25 14:05:25 +0300

    Add event for button

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,9 +3,8 @@ sap.ui.define([
  ], function (Controller) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-       onShowHello : function () {
-          // show a native JavaScript alert
-          alert("Hello World");
+       onShowHello : function (oEvent) {
+          alert(oEvent.getSource().getText());
        }
     });
  });
